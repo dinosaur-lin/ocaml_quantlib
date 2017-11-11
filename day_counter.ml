32ffaa082b81234833de2b40b894e7ba074298cc
Yunfeng Lin <yunfeng.lin04@gmail.com>
2017-11-12 03:58:34 +1100

    refactoring

--- day_counter.ml
+++ day_counter.ml
@@ -5,38 +5,37 @@ sig
     val day_count: Date.t -> Date.t -> int
 end
 
-module USA_daycount: Daycount = struct 
-    let day_count d1 d2 = let dd1 = Date.day d1 in
-                          let dd2 = Date.day d2 in
-                          let mm1 = Date.month d1 |> Month.to_int in
-                          let mm2 = Date.month d2 |> Month.to_int in
-                          let yy1 = Date.year d1 in 
-                          let yy2 = Date.year d2 in 
+module Daycount_utils = struct
+    let extract_day_month_year d1 d2 = (
+            Date.year d1,
+            Date.year d2,
+            Date.month d1 |> Month.to_int,
+            Date.month d2 |> Month.to_int,
+            Date.day d1,
+            Date.day d2
+        )    
+
+    let thirdy_360_day_count y1 y2 m1 m2 d1 d2 =
+         360 * (y2 - y1) + 30 * (m2 - m1 - 1) + (max 0 30 - d1) + (min 30 d2)
+end
+
+module USA_daycount: Daycount = struct     
+    let day_count d1 d2 = let (yy1,yy2,mm1,mm2,dd1,dd2) = Daycount_utils.extract_day_month_year d1 d2 in                          
                           let (ddd2,mmm2) = if dd2 = 31 && dd1 < 30 then (1, mm2+1) else (dd2, mm2) in
-                          360*(yy2-yy1)+30*(mmm2-mm1-1)+(max 0 30-dd1)+(min 30 ddd2)
+                          Daycount_utils.thirdy_360_day_count yy1 yy2 mm1 mmm2 dd1 ddd2
 end
 
 module EU_daycount: Daycount = struct 
-    let day_count d1 d2 = let dd1 = Date.day d1 in
-                          let dd2 = Date.day d2 in
-                          let mm1 = Date.month d1 |> Month.to_int in
-                          let mm2 = Date.month d2 |> Month.to_int in
-                          let yy1 = Date.year d1 in 
-                          let yy2 = Date.year d2 in                           
-                          360*(yy2-yy1)+30*(mm2-mm1-1)+(max 0 30-dd1)+(min 30 dd2)
+    let day_count d1 d2 = let (yy1,yy2,mm1,mm2,dd1,dd2) = Daycount_utils.extract_day_month_year d1 d2 in                           
+                          Daycount_utils.thirdy_360_day_count yy1 yy2 mm1 mm2 dd1 dd2
 end
 
 module IT_daycount: Daycount = struct 
     let modify_day m d = if m = 2 && d > 27 then 30 else d
-    let day_count d1 d2 = let dd1 = Date.day d1 in
-                          let dd2 = Date.day d2 in
-                          let mm1 = Date.month d1 |> Month.to_int in
-                          let mm2 = Date.month d2 |> Month.to_int in 
+    let day_count d1 d2 = let (yy1,yy2,mm1,mm2,dd1,dd2) = Daycount_utils.extract_day_month_year d1 d2 in
                           let ddd1 = modify_day mm1 dd1 in
-                          let ddd2 = modify_day mm2 dd2 in                                                    
-                          let yy1 = Date.year d1 in 
-                          let yy2 = Date.year d2 in                           
-                          360*(yy2-yy1)+30*(mm2-mm1-1)+(max 0 30-ddd1)+(min 30 ddd2)
+                          let ddd2 = modify_day mm2 dd2 in
+                          Daycount_utils.thirdy_360_day_count yy1 yy2 mm1 mm2 ddd1 ddd2
 end
 
 module type Daycounter = sig
@@ -52,4 +51,3 @@ end
 module US_thirty_360 = Thirty_360(USA_daycount)
 module EU_thirty_360 = Thirty_360(EU_daycount)
 module IT_thirty_360 = Thirty_360(IT_daycount)
-
