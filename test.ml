615dd2cecfa3def4c6a2332845a653acd00cada3
Yunfeng Lin <yunfeng.lin04@gmail.com>
2017-11-12 15:14:55 +1100

    clear API

--- test.ml
+++ test.ml
@@ -14,12 +14,12 @@ let end_dates = [
   Date.create_exn ~y:2007 ~m:Month.Feb ~d:28;
 ]
 
-let expected = [180; 180; 177]  
+let expected = [180; 180; 178]  
 
 let us360 start_dates end_dates expected = 
   let start_ends = List.zip_exn start_dates end_dates in
   let start_ends_expected = List.zip_exn start_ends expected in
-  List.map start_ends_expected (fun ((s,e),exp) -> fun () -> Alcotest.(check int) "Check us360" exp (US_thirty_360.day_count s e))   
+  List.map start_ends_expected (fun ((s,e),exp) -> fun () -> Alcotest.(check int) "Check us360" exp (day_count USThirty360 s e))   
 
 let test_set = 
   List.map (us360 start_dates end_dates expected) (fun t -> "us360", `Quick, t)  
