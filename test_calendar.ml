eb815f9cfcc79ec32236a389cae5498fee6de4ee
Yunfeng Lin <yunfeng.lin04@gmail.com>
2017-11-15 14:16:43 +1100

    implement day adjuster

--- test_calendar.ml
+++ test_calendar.ml
@@ -1,6 +1,6 @@
 open! Core_kernel
 
 let () = 
-  (Business_calendar.adjust Business_calendar.USGovernmentBondCalendar Business_day_convention.Unadjusted (Date.today Time.Zone.utc)) 
+  (Calendar.adjust Calendar.USGovernmentBondCalendar Business_day_convention.Following (Date.create_exn ~y:2017 ~m:Month.Nov ~d:11))   
   |> Date.to_string
   |> print_endline
